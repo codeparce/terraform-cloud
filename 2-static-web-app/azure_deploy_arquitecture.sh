e14835514c312357eeff758757bd418f8cbcfa33
code parce <codeparcelts@gmail.com>
2026-02-06 12:46:57 -0500

    DEVOPS : NEW : SHELL FOR STEPS

--- 2-static-web-app/azure_deploy_arquitecture.sh
+++ 2-static-web-app/azure_deploy_arquitecture.sh
@@ -3,25 +3,29 @@ set -euo pipefail
 
 echo "Azure Login CLI"
 
-ls -la ./
+#ls -la ./
 
 az login --service-principal \
   -u "$ARM_CLIENT_ID" \
   -p "$ARM_CLIENT_SECRET" \
-  --tenant "$ARM_TENANT_ID"
+  --tenant "$ARM_TENANT_ID" < login.log
 
 
 echo "Set the subscription"
 
-az account set --subscription "$ARM_SUBSCRIPTION_ID"
+az account set --subscription "$ARM_SUBSCRIPTION_ID" < login.log
 
 echo "Set environment"
 
 source .env 
 
+#echo "Environment variables loaded"
 
-echo "Environment variables loaded"
+#echo "TF_VAR_resource_group_name : $TF_VAR_resource_group_name"  
+#echo "TF_VAR_location : $TF_VAR_location"  
+#echo "TF_VAR_static_app_name : $TF_VAR_static_app_name"  
 
-echo "TF_VAR_resource_group_name : $TF_VAR_resource_group_name"  
-echo "TF_VAR_location : $TF_VAR_location"  
-echo "TF_VAR_static_app_name : $TF_VAR_static_app_name"  
+terraform 
+
+#terraform init -input=false
+#terraform apply -auto-approve
