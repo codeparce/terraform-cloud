18cfe28c5dce22998686c1f3bfce417e6db13eb7
code parce <codeparcelts@gmail.com>
2026-02-06 13:22:18 -0500

    DEVOPS : NEW : SHELL FOR STEPS

--- 2-static-web-app/azure_deploy_arquitecture.sh
+++ 2-static-web-app/azure_deploy_arquitecture.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
-LOG_FILE="general-logs.log"
 LOG_AZ="azure-logs.log"
-LOG_ERR=error.log 
-
-touch $LOG_AZ $LOG_ERR
-
-# Guardar TODO (stdout + stderr), append
-
-exec > >(tee -a "$LOG_FILE") 2> >(tee -a "$LOG_ERR" >&2)
-
-on_error() {
-  echo "❌ ERROR detected. Showing logs:"
-  echo "---------------------------------"
-  cat $LOG_ERR
-  echo "---------------------------------"
-}
-
-trap on_error ERR
 
 echo "Azure Login CLI"
 
@@ -27,21 +9,26 @@ az login --service-principal \
   -p "$ARM_CLIENT_SECRET" \
   --tenant "$ARM_TENANT_ID"  > "$LOG_AZ" 2>&1
 
+echo "✅ Azure Login CLI"
+
 echo "Set the subscription"
 
 az account set --subscription "$ARM_SUBSCRIPTION_ID" > "$LOG_AZ" 2>&1
+
+echo "✅ Set the subscription"
 
 echo "Set environment"
 
 source .env 
 
-#echo "Environment variables loaded"
+echo "✅ Set environment"
 
-#echo "TF_VAR_resource_group_name : $TF_VAR_resource_group_name"  
-#echo "TF_VAR_location : $TF_VAR_location"  
-#echo "TF_VAR_static_app_name : $TF_VAR_static_app_name"  
+
+echo "terraform init and apply"
 
 terraform 
+
+echo "✅ terraform init and apply"
 
 #terraform init -input=false
 #terraform apply -auto-approve
