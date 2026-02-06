5a586901c3c916b461c868c2b95ac208079cf537
code parce <codeparcelts@gmail.com>
2026-02-06 16:06:40 -0500

    DEVOPS : NEW : BACKEND AND CI/CD

--- 2-static-web-app/azure_deploy_arquitecture.sh
+++ 2-static-web-app/azure_deploy_arquitecture.sh
@@ -5,13 +5,12 @@ export TF_VAR_static_app_name
 export TF_VAR_location
 
 LOG_AZ="azure-logs.log"
-
 touch $LOG_AZ 
-
+ 
 source ../logs.sh
 source ./azure_login.sh
 
- 
+
 source .env 
 
 echo "✅ Set environment"
@@ -28,3 +27,14 @@ echo "✅ terraform plan"
 terraform apply -lock-timeout=1m -auto-approve
 
 echo "✅ terraform apply"
+
+NORMALIZED_NAME=$(echo "$TF_VAR_static_app_name" \
+  | tr '[:upper:]' '[:lower:]' \
+  | tr '/' '-' \
+  | cut -c1-60)
+
+az staticwebapp secrets list \
+--name $NORMALIZED_NAME \
+--resource-group $TF_VAR_resource_group_name > $FILE_JSON
+
+echo "✅ SUCESS: Secrets exported to $FILE_JSON"
