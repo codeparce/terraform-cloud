726f13c684b006b528cf9456b58a95832530cc77
code parce <codeparcelts@gmail.com>
2026-02-06 13:24:42 -0500

    DEVOPS : NEW : SHELL FOR STEPS

---
+++ logs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+LOG_FILE="general-logs.log"
+LOG_ERR=error.log 
+
+touch $LOG_AZ $LOG_ERR
+
+# Guardar TODO (stdout + stderr), append
+
+exec > >(tee -a "$LOG_FILE") 2> >(tee -a "$LOG_ERR" >&2)
+
+on_error() {
+  echo "❌ ERROR detected. Showing logs:"
+  echo "---------------------------------"
+  cat $LOG_ERR
+  echo "---------------------------------"
+}
+
+trap on_error ERR
