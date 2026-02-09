7d45a21435779813c1ead87b0fabeccfa888329e
code parce <codeparcelts@gmail.com>
2026-02-08 22:53:50 -0500

    DEVOPS : NEW : nueva variable para el .env

--- 4-container-app/main.sh
+++ 4-container-app/main.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
- 
+LOG_AZ="azure-logs.log"
+touch $LOG_AZ 
+
 source ../logs.sh
 source ../azure_login.sh
 source ../tf_deploy.sh
-
-LOG_AZ="azure-logs.log"
-touch $LOG_AZ 
 
 
 enviroment 
