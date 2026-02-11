5cdb35a52c8d8494e0b735e896317d3805b0ab58
code parce <codeparcelts@gmail.com>
2026-02-11 01:04:30 -0500

    DEVOPS : FIX : añadir lock false

--- logs.sh
+++ logs.sh
@@ -8,13 +8,20 @@ touch $LOG_FILE $LOG_ERR
 
 # Guardar TODO (stdout + stderr), append
 
-exec > >(tee -a "$LOG_FILE") 2> >(tee -a "$LOG_ERR" >&2)
+exec > >(tee -a "$LOG_FILE") 2>> "$LOG_ERR"
 
 on_error() {
   echo "❌ ERROR detected. Showing logs:"
   echo "---------------------------------"
   cat $LOG_ERR
   echo "---------------------------------"
+
+  if [[ -f .terraform/lock_id.txt && -s .terraform/lock_id.txt ]]; then
+    echo "Attempting to force-unlock Terraform state..."
+    terraform force-unlock "$(cat .terraform/lock_id.txt)"
+  else
+    echo "No lock ID found, skipping force-unlock"
+  fi
 }
 
 trap on_error ERR
--- tf_deploy.sh
+++ tf_deploy.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 terraform_shell () {
-    echo "✅ Set environment"
 
     terraform init \
-    -backend-config="key=${GITHUB_REPOSITORY}-${GITHUB_REF_NAME}.tfstate" -reconfigure -lock=false
+    -backend-config="key=${GITHUB_REPOSITORY}-${GITHUB_REF_NAME}.tfstate" -reconfigure
 
     echo "✅ terraform init"
 
-    terraform plan -lock=false
+    terraform plan -lock-timeout=0s 2>&1 | tee plan.out || true
+
+    grep 'ID:' plan.out | awk '{print $2}' > .terraform/lock_id.txt || true
 
     echo "✅ terraform plan"
 
-    terraform apply -auto-approve -lock=false
+    terraform apply  -auto-approve
 
     echo "✅ terraform apply"
 }
