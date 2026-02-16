db53afa40d8736160987379027d5940b61c82ca3
code parce <codeparcelts@gmail.com>
2026-02-15 21:54:06 -0500

    DEVOPS : NEW : Servicio de configuracion aplicaciones

--- 5-container-app/main.sh
+++ 5-container-app/main.sh
@@ -8,17 +8,16 @@ LOG_AZ="azure-logs.log"
 touch $LOG_AZ 
 
 get_envs () {
+    echo "Descargando configuracion"
 
+    az appconfig kv export --name "${TF_VAR_app_config_name}" --label "${GITHUB_REPOSITORY_NAME}-${GITHUB_REF_NAME}" --destination file --path ./config.json --format json --skip-feature 
 }
 
 azure_login
 
 enviroment 
 
-echo "Descargando configuracion"
-
-az appconfig kv export --name "${TF_VAR_app_config_name}" --label "${GITHUB_REPOSITORY_NAME}-${GITHUB_REF_NAME}" --destination file --path ./config.json --format json --skip-feature 
-
+get_envs
 ls
 
 terraform_shell
