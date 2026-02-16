c663d70e5904fa5a8b0993a41f6a8482d53215e0
code parce <codeparcelts@gmail.com>
2026-02-15 21:52:50 -0500

    DEVOPS : NEW : Servicio de configuracion aplicaciones

--- 5-container-app/main.sh
+++ 5-container-app/main.sh
@@ -17,7 +17,8 @@ enviroment
 
 echo "Descargando configuracion"
 
-az appconfig kv export --name $TF_VAR_app_config_name --label ${GITHUB_REPOSITORY_NAME}-${GITHUB_REF_NAME} --destination file --path ./config.json --format json --skip-feature 
+az appconfig kv export --name "${TF_VAR_app_config_name}" --label "${GITHUB_REPOSITORY_NAME}-${GITHUB_REF_NAME}" --destination file --path ./config.json --format json --skip-feature 
+
 ls
 
 terraform_shell
