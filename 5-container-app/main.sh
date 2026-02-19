ad9224765bb5c07eec3d3092fbd47c9b8e881c5e
code parce <codeparcelts@gmail.com>
2026-02-19 00:02:51 -0500

    DEVOPS: NEW : añadir configuracion de variables con azure

--- 5-container-app/main.sh
+++ 5-container-app/main.sh
@@ -5,22 +5,19 @@ source ../azure_login.sh
 source ../tf_deploy.sh
 
 LOG_AZ="azure-logs.log"
-touch $LOG_AZ 
 
-get_envs () {
-    echo "Descargando configuracion"
-    az appconfig kv export --name $TF_VAR_app_config_name --label $GITHUB_REPOSITORY_NAME-$GITHUB_REF_NAME --destination file --path ./config.json --format json --skip-feature --yes
-    cat config.json
-}
+touch $LOG_AZ 
 
 azure_login
 
 enviroment 
 
 get_envs
-ls
 
 terraform_shell
 
 
-
+get_envs () {
+    az appconfig kv export --name $TF_VAR_app_config_name --label $GITHUB_REPOSITORY_NAME-$GITHUB_REF_NAME --destination file --path ./config.json --format json --skip-feature --yes
+    echo "✅ Configuracion descargada"
+}
