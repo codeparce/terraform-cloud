500315f83c3173224114b7211a4903724c13241c
code parce <codeparcelts@gmail.com>
2026-02-21 11:33:02 -0500

    DEVOPS : NEW : IMPLEMENTACION DEL VAULT

--- 3-functions/upload_zip.sh
+++ 3-functions/upload_zip.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -x
 
 source ../logs.sh
 source ../gcp_login.sh
@@ -7,20 +6,12 @@ source ../vault_env.sh
 
 LOG_AZ="gcp-logs.log"
 
-sudo snap install vault
-
-vault -version
-
-
-#mkdir -p secret
 touch $LOG_AZ 
 
 loginVault
 
 getSecretJson service-account-terraform-gcp 
 
-getSecret $GITHUB_REPOSITORY_NAME-$GITHUB_REF_NAME .env
+getSecret $GITHUB_REPOSITORY_NAME-$GITHUB_REF_NAME .env.tf
 
-cat .env
-
-enviroment .env
+enviroment .env.tf
--- vault_env.sh
+++ vault_env.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
+sudo snap install vault
+
 
 loginVault(){
     vault login -method=userpass username=$VAULT_USER password=$VAULT_PASS
+    echo "✅ Login vault"
 }
 
 getSecret(){
     vault kv get -format=json secret/"$1"   | jq -r '.data.data | to_entries[] | "\(.key)=\(.value)"' > $2
+    echo "✅ get secret vault"
 }
 
 
 getSecretJson(){
     vault kv get -format=json secret/"$1"  > key.json
-    pwd
-    ls
+    echo "✅ get service account"
 }
