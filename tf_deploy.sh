de9ac9dc8946e677659acb83b0325018a8f1359e
code parce <codeparcelts@gmail.com>
2026-02-21 12:59:08 -0500

    DEVOPS : NEW : IMPLEMENTACION DEL VAULT

--- tf_deploy.sh
+++ tf_deploy.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 terraform_shell () {
-    echo "✅ Set environment"
-
     terraform init -backend-config="prefix=${BACKEND_NAME}" -reconfigure
 
     echo "✅ terraform init"
 
-    terraform plan -lock-timeout=1m
-
-    echo "✅ terraform plan"
-
-    terraform apply -lock-timeout=1m -auto-approve
+    terraform apply -auto-approve
 
     echo "✅ terraform apply"
 }
