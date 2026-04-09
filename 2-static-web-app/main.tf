1f29f660715c14db0ab662771d3cc6e0e8fda295
codeparce <codeparcelts@gmail.com>
2026-04-08 22:12:22 -0500

    DEVOPS : FIX : Correcion de scripts

--- 2-static-web-app/main.tf
+++ 2-static-web-app/main.tf
@@ -5,7 +5,7 @@ data "azurerm_resource_group" "rg" {
 resource "azurerm_static_web_app" "static-web-app" {
   name                = var.STATIC_APP_NAME
   resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
+  location            = var.LOCATION
   sku_tier            = "Free"
   sku_size            = "Free"
 }
