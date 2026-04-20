c71f88e5870d912d0522f3b5d85f13e0eacefa26
codeparce <codeparcelts@gmail.com>
2026-04-20 12:08:27 -0500

    DEVOPS : NEW : Nueva practica de servicios DB SQL server mysql

---
+++ 8-db/sql-server/variable.tf
@@ -0,0 +1,42 @@
+variable "LOCATION" {
+  default     = "westus2"
+  description = "locacion del Resource Group"
+  type        = string
+}
+
+variable "RG_NAME" {
+  default     = "lab-002-sql-main"
+  description = "nombre del Resource Group"
+  type        = string
+}
+
+variable "PROJECT_NAME" {
+  default     = "sql-lab"
+  description = "nombre del proyecto para etiquetado"
+  type        = string
+}
+
+variable "OWNER" {
+  default     = "codeparce"
+  description = "nombre del propietario para etiquetado"
+  type        = string
+}
+
+variable "SQL_PASSWORD" {
+  default     = "Password123!"
+  description = "Contraseña del administrador de SQL Server"
+  type        = string
+}
+
+variable "SQL_USERNAME" {
+  default     = "sqladmin"
+  description = "Nombre de usuario del administrador de SQL Server"
+  type = string
+}
+
+variable "IP_TEST" {
+  default     = "tu.ip.publica.aqui"
+  description = "Dirección IP de prueba para el firewall de SQL Server"
+  type = string
+}
+## curl ipinfo.io/ip en windows para ver la ip publica
