c50f4ed62cc8c087a9c60b7dc5e6debb755c1b08
codeparce <codeparcelts@gmail.com>
2026-04-23 12:43:34 -0500

    DEVOPS : FIX : Correccion de logs

--- azure/utils_deploy.sh
+++ azure/utils_deploy.sh
@@ -7,11 +7,11 @@ terraform_shell () { ## separar en funciones para mayor optimzacion en los jobs
 
     echo "✅ terraform init"
 
-    terraform plan -out=tfplan   
+    terraform plan -out=tfplan -lock=false  2>&1 
     
     echo "✅ terraform plan"
 
-    terraform apply -input=false tfplan  
+    terraform apply -input=false tfplan  2>&1 
     #terraform apply -auto-approve -input=false 2>&1 | \
     #grep -vE '^\s*(name|value)\s*='
 
