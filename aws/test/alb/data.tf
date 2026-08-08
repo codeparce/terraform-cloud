46dd4dcde3591ee7ec4ec2c95f87bf2ab37fd567
codeparce <codeparcelts@gmail.com>
2026-08-08 13:17:58 -0500

    Devops: New : Nuevo daigrama VPC +ALB +ACS

--- aws/test/alb/data.tf
+++
@@ -1,8 +0,0 @@
-data "http" "my_ip" {
-  url = "https://checkip.amazonaws.com"
-}
-
-
-data "aws_lambda_function" "api" {
-  function_name = "lambd-alb"
-}
--- aws/test/ec2/output.tf
+++
@@ -1,8 +0,0 @@
-output "allec2" {
-  value = module.ec2_instance.vm1
-}
-
-
-output "ec2_id" {
-  value = module.ec2_instance.vm1.id
-}
